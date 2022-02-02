1f7c225c94c34252e5dd2454d66692d9cff13538
ahub <ahub@riseup.net>
2022-02-02 09:57:26 +0100

    Tweak screenshake values

--- src/game/life.h
+++ src/game/life.h
@@ -4,11 +4,12 @@
 
 #include "factory.h"
 #include "shake.h"
+#include <cstdlib>
 
 struct Life : public Component {
     inline static String name = "Life";
 
-    int hp{10};
+    int hp{3};
 };
 
 class LifeSystem : public System {
@@ -28,8 +29,17 @@ class LifeSystem : public System {
             engine->removeEntity(entity);
 
             if (!Context::get().cameraEntity->has<Shake>()) {
-
                 Context::get().cameraEntity->add<Shake>();
+
+                //TODO: Maybe this should be in the constructor for Shake 
+                //instead with a static variable making it bigger every Nth 
+                //trigger.
+                //I'd also add a freeze-frame effect as well, but that's 
+                //another topic.
+                if(rand() % 100 >= 70) {
+                  Context::get().cameraEntity->get<Shake>().intensity += 0.8f;
+                  Context::get().cameraEntity->get<Shake>().duration += 1.0f;
+                }
             }
         }
     }
